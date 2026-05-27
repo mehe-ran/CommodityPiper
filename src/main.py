8ccd940700522905c35a77cfe34175fb7cc4f68b
mehe-ran <taraporewalameheran@gmail.com>
2026-05-27 14:22:11 +1000

    add post endpoint for data ingestion pipeline

--- src/main.py
+++ src/main.py
@@ -28,3 +28,8 @@ def read_locations(skip: int = 0, limit: int = 100, db: Session = Depends(get_db
 @app.get("/commodities/", response_model=List[schemas.Commodity])
 def read_commodities(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return crud.get_commodities(db, skip=skip, limit=limit)
+
+# endpoint to ingest daily market price data
+@app.post("/prices/", response_model=schemas.DailyPrice)
+def create_price_record(price: schemas.DailyPriceCreate, db: Session = Depends(get_db)):
+    return crud.create_daily_price(db=db, price_schema=price)
