30ecb44d330c5537cf160d1a201110e593985d96
mehe-ran <taraporewalameheran@gmail.com>
2026-06-04 09:22:14 +1000

    add central logging configuration

---
+++ src/logger.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+# configure standard enterprise logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+
+logger = logging.getLogger("commodity_piper")
